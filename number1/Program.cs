1793c980a7f97364a92eebc930a3998278d1db4c
Badri <9299547077@mail.ru>
2022-12-03 22:11:58 +0300

    Показать 2-ю цифру трёхзначного

---
+++ number1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите число от 100 до 999 включительно");
+int num = Convert.ToInt32(System.Console.ReadLine());
+int sum = (num / 10) % 10;
+System.Console.WriteLine(sum);
